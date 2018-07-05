93dd352eda4b1b6dcc1ff19f2522b6e5199cb7a3
Dan Esparza <esparza.dan@gmail.com>
2018-07-05 15:04:16 -0400

    Initial code commit
    Added word lists and random phrase generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//  Import our adjectives and nouns:
+import Personality from './words/Personality';
+import Sensory from './words/Sensory';
+import Nouns from './words/Nouns';
 
 class App extends Component {
   render() {
+
+    //  Select a random noun:
+    let nounIndex = Math.floor(Math.random()*Nouns.length);
+    let noun = Nouns[nounIndex].toLowerCase();
+
+    //  Select a random sensory adjective:
+    let senseIndex1 = Math.floor(Math.random()*Sensory.length);
+    let sensoryAdjective1 = Sensory[senseIndex1].toLowerCase();
+
+    let senseIndex2 = Math.floor(Math.random()*Sensory.length);
+    let sensoryAdjective2 = Sensory[senseIndex2].toLowerCase();
+
+    //  Select a random personality adjective:
+    let personalityIndex = Math.floor(Math.random()*Personality.length);
+    let personalityAdjective = Personality[personalityIndex].toLowerCase();
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">You {sensoryAdjective1}, {personalityAdjective} and {sensoryAdjective2} {noun}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Compliments like Leslie Knope to Ann Perkins
         </p>
       </div>
     );
